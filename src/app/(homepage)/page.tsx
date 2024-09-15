f6e43c441108dec6a850e72f5b933e99d4ee663a
xVelkow <redamaktoum@gmail.com>
2024-09-15 18:00:01 +0100

    adding justify-around to the keywords below hero section in homepage

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -8,9 +8,14 @@ export default function Home() {
     return (
         <main>
             <Hero />
-            <div className="hidden xl:block bg-pink-400 text-white py-4 text-center text-lg baskerville font-bold">FITNESS COACHING FOR WOMEN ✨ PERSONALIZED NUTRITION PLANS ✨ HEALTHY LIFESTYLE GUIDANCE</div>
+            <div className="hidden xl:flex xl:justify-around bg-pink-400 text-white py-4 text-center text-lg baskerville font-bold">
+                <span>FITNESS COACHING FOR WOMEN</span>
+                <span>✨</span>
+                <span>PERSONALIZED NUTRITION PLANS</span>
+                <span>✨</span>
+                <span>HEALTHY LIFESTYLE GUIDANCE</span>
+            </div>
             <About />
-            {/* <div className="bg-pink-400 text-white py-4 text-center text-lg baskerville">YOU GOT THIS</div> */}
             <Services />
             <Newsletter />
             <Footer />
