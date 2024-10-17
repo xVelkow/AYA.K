d26ee1498e9e7dd1951848e5f1e672ad73bb2e25
xVelkow <redamaktoum@gmail.com>
2024-10-17 15:26:52 +0100

    adding first blog

--- src/app/blogs/1/page.tsx
+++ src/app/blogs/1/page.tsx
@@ -10,10 +10,10 @@ import Button from "../Button";
 
 export default function Page() {
     return <Blog>
-        <Headline>Why Training is Important for Women’s Health</Headline>
+        <Headline>Why Training is Important for Women&#8217;s Health</Headline>
         <BlogImage src="/blogs/1.jpg" alt="im" width={300} height={300} />
         <Content>
-            <Paragraph>Fitness is often seen as a form of self-care, but let’s be honest—self-care can sometimes feel like a luxury, squeezed in when time allows. Despite its importance, putting self-care into action can be challenging. When we treat it like just another item on our to-do list, we risk missing its true value.</Paragraph>
+            <Paragraph>Fitness is often seen as a form of self-care, but let&#8217;s be honest—self-care can sometimes feel like a luxury, squeezed in when time allows. Despite its importance, putting self-care into action can be challenging. When we treat it like just another item on our to-do list, we risk missing its true value.</Paragraph>
             <Title>hello everyone</Title>
             <Paragraph>Exercise goes beyond enhancing appearances; it nurtures physical and mental well-being. While beneficial for everyone, women especially reap numerous health rewards:</Paragraph>
             <ParagraphGroup>
@@ -28,9 +28,9 @@ export default function Page() {
             </ParagraphGroup>
             <Paragraph>These insights highlight a simple truth: exercise is fundamental for women's health.</Paragraph>
             <Paragraph>I encourage you to honor and care for your body, treating it with the same dedication you give other aspects of your life. Exercise isn't just self-care; it's an essential component of a balanced lifestyle.</Paragraph>
-            <SubTitle>“I really don’t have time to workout.”</SubTitle>
+            <SubTitle>&#8220;I really don&#8217;t have time to workout.&#8221;</SubTitle>
             <Paragraph>I understand finding time for exercise can seem daunting. However, prioritizing your well-being amidst life's demands is crucial. Whether it's an hour daily or just 15 minutes every other day, making a commitment to yourself is vital. Research indicates that scheduling and forming healthy habits can reduce stress and enhance mental well-being. By prioritizing exercise in your schedule, you increase the likelihood of reaping its benefits.</Paragraph>
-            <SubTitle>“But, I don’t know where to start?”</SubTitle>
+            <SubTitle>&#8220;But, I don&#8217;t know where to start?&#8221;</SubTitle>
             <Paragraph>Here is where I come in. As a certified personal trainer and Nutrition specialist, I offer online coaching programs that integrate both physical training and nutrition.</Paragraph>
             <Paragraph><strong>My goal is to help you achieve your goals quickly and healthily.</strong></Paragraph>
             <Button href="/services">Choose Your Plan NOW!</Button>
